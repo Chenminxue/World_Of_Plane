11e54edb9716157f9a355e48682d99eef4f3c90d
minxue <2224239003@qq.com>
2021-12-28 19:38:23 +0100

    second commit

--- plane.py
+++ plane.py
@@ -20,13 +20,25 @@ class PlaneGame(object):
         # Enemy and enemy group.
         self.__create_sprites()
 
+        # Set timer.
+        pygame.time.set_timer(CREATE_ENEMY_EVENT, 1000)
+        pygame.time.set_timer(HERO_FIRE_EVENT, 500)
+
     def __create_sprites(self):
         # Create background sprites. Two background images refresh alternately.
         bg_1 = Background()
         bg_2 = Background(True)
-        bg_2.rect.y = -bg_2.rect.height
+        # bg_2.rect.y = -bg_2.rect.height
 
         self.back_group = pygame.sprite.Group(bg_1, bg_2)
+
+        # Create enemy sprites.
+        self.enemy_group = pygame.sprite.Group()
+
+        # Create Hero sprite.
+        self.hero = Hero()
+        self.hero_group = pygame.sprite.Group(self.hero)
+
 
     def start_game(self):
         print("Game start.")
@@ -48,18 +60,61 @@ class PlaneGame(object):
             pygame.display.update()
 
     def __event_handler(self):
+
         for event in pygame.event.get():
 
             # Quit game.
             if event.type == pygame.QUIT:
                 PlaneGame.__game_over()
+            elif event.type == CREATE_ENEMY_EVENT:
+                print("Creating enemies...")
+                # Create enemy sprites.
+                enemy = Enemy()
+
+                # Add enemies to group.
+                self.enemy_group.add(enemy)
+            elif event.type == HERO_FIRE_EVENT:
+                self.hero.fire()
+
+        # Press Keyboard.
+        keys_pressed = pygame.key.get_pressed()
+        # Move to right.
+        if keys_pressed[pygame.K_RIGHT]:
+            self.hero.speed = 1
+        # Move to left.
+        elif keys_pressed[pygame.K_LEFT]:
+            self.hero.speed = -1
+        # Stop.
+        else:
+            self.hero.speed = 0
+
 
     def __collision_detection(self):
-        pass
+
+        #  Bullet destroy enemies.
+        pygame.sprite.groupcollide(self.hero.bullets, self.enemy_group, True, True)
+        # Check collision.
+        enemies = pygame.sprite.spritecollide(self.hero, self.enemy_group, True)
+        if len(enemies) > 0:
+
+            # Hero dead.
+            self.hero.kill()
+
+            # Game over.
+            PlaneGame.__game_over()
 
     def __update_sprites(self):
         self.back_group.update()
         self.back_group.draw(self.screen)
+
+        self.enemy_group.update()
+        self.enemy_group.draw(self.screen)
+
+        self.hero_group.update()
+        self.hero_group.draw(self.screen)
+
+        self.hero.bullets.update()
+        self.hero.bullets.draw(self.screen)
 
     @staticmethod
     def __game_over():
--- plane_sprites.py
+++ plane_sprites.py
@@ -1,11 +1,16 @@
 """ This is the class for sprites. """
 
+import random
 import pygame
 
 # Window size.
 SCREEN_RECT = pygame.Rect(0, 0, 480, 700)
-# Frame rate
+# Frame rate.
 FRAME_RATE = 60
+# Enemy event.
+CREATE_ENEMY_EVENT = pygame.USEREVENT
+# Fire event
+HERO_FIRE_EVENT = pygame.USEREVENT + 1
 
 
 class GameSprites(pygame.sprite.Sprite):
@@ -40,5 +45,106 @@ class Background(GameSprites):
 
         if self.rect.y >= SCREEN_RECT.height:
             self.rect.y = -self.rect.height
+
+
+class Enemy(GameSprites):
+    """ Enemy sprites. """
+
+    def __init__(self):
+
+        # Create enemy sprites.
+        super().__init__("./images/enemy1.png")
+
+        # Random speed.
+        self.speed = random.randint(1,3)
+
+        # Random position.
+        self.rect.bottom = 0
+
+        max_x = SCREEN_RECT.width - self.rect.width
+        self.rect.x = random.randint(0, max_x)
+
+    def update(self):
+
+        # Move vertically.
+        super().update()
+
+        # Delete outside enemies.
+        if self.rect.y >= SCREEN_RECT.height:
+            # print("Outside the screen, deleting enemies...")
+
+            # Delete enemies.
+            self.kill()
+
+    def __del__(self):
+        # print("Enemy is dead... at %s" % self.rect)
+        pass
+
+
+class Hero(GameSprites):
+    """ Hero sprite. """
+
+    def __init__(self):
+
+        # Set up image and speed.
+        super().__init__("./images/me1.png", 0)
+
+        # Set up position.
+        self.rect.centerx = SCREEN_RECT.centerx
+        self.rect.bottom = SCREEN_RECT.bottom - 120
+
+        # Create bullet sprites.
+        self.bullets = pygame.sprite.Group()
+
+    def update(self):
+
+        # Move horizontally.
+        self.rect.x += self.speed
+
+        # Make sure the hero can not escape from the screen.
+        if self.rect.x < 0:
+            self.rect.x = 0
+        elif self.rect.x > 480 - self.rect.width:
+            self.rect.x = 480 - self.rect.width
+
+    def fire(self):
+        """ Fire class. """
+
+        for i in (0, 1, 2):
+
+
+            # Create bullet sprites.
+            bullet = Bullet()
+
+            # Set up bullet position
+            bullet.rect.bottom = self.rect.y - 20
+            bullet.rect.centerx = self.rect.centerx + (i - 1) * 10
+
+            # Add sprites into Group
+            self.bullets.add(bullet)
+
+            print("Fire!")
+
+
+class Bullet(GameSprites):
+    """" Bullet class. """
+
+    def __init__(self):
+
+        # Set up bullet image and speed.
+        super().__init__("./images/bullet2.png", -2)
+
+    def update(self):
+
+        # Move vertically.
+        super().update()
+
+        # Delete bullet when outside the screen.
+        if self.rect.bottom < 0:
+            self.kill()
+
+
+    def __del__(self):
+        print("Bullet deleted...")
 
 
